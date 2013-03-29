537476ab4d88dbce7f894e07abecf9afdffb256f
Alex Miller <alexmiller@gmail.com>
2013-03-29 13:59:41 -0700

    fleshed out some route-controller bindings

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -5,13 +5,37 @@ function PostController() {
   // ??
 }
 
-PostController.prototype.all = function(err, res) {
+PostController.prototype.all = function(req, res) {
   Post.all(function(result) {
     res.render('posts', {posts: result, message: "hello!"});
   });
 };
 
+PostController.prototype.category = function(req, res) {
+
+};
+
+PostController.prototype.feed = function(req, res) {
+
+};
+
+PostController.prototype.unreadForFeed = function(req, res) {
+
+};
+
+PostController.prototype.setCategory = function(req, res) {
+
+};
+
+PostController.prototype.destroy = function(req, res) {
+
+};
+
 exports.setup = function(app) {
   var controller = new PostController();
   app.get('/posts', controller.all);
+  app.get('/category', controller.category);
+  app.get('/feed', controller.feed);
+  app.post('/set_category', controller.setCategory);
+  app.destroy('/post', controller.destroy);
 };
